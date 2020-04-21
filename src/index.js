e4763e04d8d852722c5889002a505ab8051a26c9
Rahul <rash235r@gmail.com>
2020-04-21 10:38:00 +0200

    chore: add cors for frontend communicaton

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+const cors = require('cors');
 const getWeather = require('./services/weather');
 
 const app = express();
 const server = http.createServer(app);
 
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: [process.env.APPLICATION_URL],
+  methods: 'GET, POST',
+};
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 server.listen(port, () => {
   console.log(`Listening at ${port}`);
 });
 
-app.use(bodyParser.json());
-
 app.get('/weather', (req, res) => {
   const callback = (error, data) => {
     if (error) {
+      res.setHeader('Content-Type', 'application/json');
       res.status(500).send(error);
       return;
     }
