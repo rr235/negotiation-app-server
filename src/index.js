cec03adea473905b0994d66e06acaa4666b41db2
Rahul <rash235r@gmail.com>
2020-04-21 12:29:49 +0200

    chore: setup server session

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const cors = require('cors');
 const getWeather = require('./services/weather');
+const { v4: uuid } = require('uuid');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const app = express();
 const server = http.createServer(app);
@@ -12,12 +15,24 @@ const corsOptions = {
   origin: [process.env.APPLICATION_URL],
   methods: 'GET, POST',
 };
+const sessionOptions = {
+  genid: () => uuid(),
+  store: new FileStore(),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+};
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use(session(sessionOptions));
 
 server.listen(port, () => {
   console.log(`Listening at ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send(req.sessionID);
 });
 
 app.get('/weather', (req, res) => {
